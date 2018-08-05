188128b40333d0e7a444a6de3f100e1edc52c02c
Igor Liferenko <igor.liferenko@gmail.com>
2018-08-05 11:17:15 +0700

    rm usb_descriptors.c

--- main.c
+++ main.c
@@ -131,6 +131,173 @@ S_line_coding line_coding;
 U8 usb_suspended = 0;
 U8 usb_connected = 0;
 Uchar rx_counter;
+typedef struct {
+  U8 bmRequestType;
+  U8 bRequest;
+  U16 wValue;
+  U16 wIndex;
+  U16 wLength;
+} S_UsbRequest;
+
+typedef struct {
+  U8 bLength;
+  U8 bDescriptorType;
+  U16 bscUSB;
+  U8 bDeviceClass;
+  U8 bDeviceSubClass;
+  U8 bDeviceProtocol;
+  U8 bMaxPacketSize0;
+  U16 idVendor;
+  U16 idProduct;
+  U16 bcdDevice;
+  U8 iManufacturer;
+  U8 iProduct;
+  U8 iSerialNumber;
+  U8 bNumConfigurations;
+} S_usb_device_descriptor;
+
+typedef struct {
+  U8 bLength;
+  U8 bDescriptorType;
+  U16 wTotalLength;
+  U8 bNumInterfaces;
+  U8 bConfigurationValue;
+  U8 iConfiguration;
+  U8 bmAttibutes;
+  U8 MaxPower;
+} S_usb_configuration_descriptor;
+
+typedef struct {
+  U8 bLength;
+  U8 bDescriptorType;
+  U8 bInterfaceNumber;
+  U8 bAlternateSetting;
+  U8 bNumEndpoints;
+  U8 bInterfaceClass;
+  U8 bInterfaceSubClass;
+  U8 bInterfaceProtocol;
+  U8 iInterface;
+} S_usb_interface_descriptor;
+
+typedef struct {
+  U8 bLength;
+  U8 bDescriptorType;
+  U8 bEndpointAddress;
+  U8 bmAttributes;
+  U16 wMaxPacketSize;
+  U8 bInterval;
+} S_usb_endpoint_descriptor;
+
+typedef struct {
+  U8 bLength;
+  U8 bDescriptorType;
+  U16 bscUSB;
+  U8 bDeviceClass;
+  U8 bDeviceSubClass;
+  U8 bDeviceProtocol;
+  U8 bMaxPacketSize0;
+  U8 bNumConfigurations;
+  U8 bReserved;
+} S_usb_device_qualifier_descriptor;
+
+typedef struct {
+  U8 bLength;
+  U8 bDescriptorType;
+  U16 wlangid;
+} S_usb_language_id;
+
+typedef struct {
+  U8 bLength;
+  U8 bDescriptorType;
+  U16 wstring[5];
+} S_usb_manufacturer_string_descriptor;
+
+typedef struct {
+  U8 bLength;
+  U8 bDescriptorType;
+  U16 wstring[16];
+} S_usb_product_string_descriptor;
+
+typedef struct {
+  U8 bLength;
+  U8 bDescriptorType;
+  U16 wstring[0x05];
+} S_usb_serial_number;
+
+typedef struct {
+  S_usb_configuration_descriptor cfg;
+  S_usb_interface_descriptor ifc0;
+  U8 CS_INTERFACE[19];
+  S_usb_endpoint_descriptor ep3;
+  S_usb_interface_descriptor ifc1;
+  S_usb_endpoint_descriptor ep1;
+  S_usb_endpoint_descriptor ep2;
+} S_usb_user_configuration_descriptor;
+
+PROGMEM const S_usb_device_descriptor usb_dev_desc = {
+  sizeof(usb_dev_desc)
+    , 0x01, (0x0200)
+    , 0x02, 0, 0, 32, (0x03EB)
+    , (0x2018)
+    , (0x1000)
+  , 0x00, 0x00, 0x00, 1
+};
+
+PROGMEM const S_usb_user_configuration_descriptor usb_conf_desc = {
+  {sizeof(S_usb_configuration_descriptor)
+   , 0x02, 0x0043, 2, 1, 0, (0x80 | 0x00)
+   , 50}
+  ,
+  {sizeof(S_usb_interface_descriptor)
+   , 0x04, 0, 0, 1, 0x02, 0x02, 0x01, 0}
+  ,
+  {0x05, 0x24, 0x00, 0x10, 0x01, 0x05, 0x24, 0x01, 0x03, 0x01, 0x04, 0x24,
+   0x02, 0x06, 0x05, 0x24, 0x06, 0x00, 0x01}
+  ,
+  {sizeof(S_usb_endpoint_descriptor)
+   , 0x05, 0x80 | 0x03, 0x03, (0x20)
+   , 0xFF}
+  ,
+  {sizeof(S_usb_interface_descriptor)
+   , 0x04, 1, 0, 2, 0x0A, 0x00, 0x00, 0}
+  ,
+  {sizeof(S_usb_endpoint_descriptor)
+   , 0x05, 0x80 | 0x01, 0x02, (0x20)
+   , 0x00}
+  ,
+  {sizeof(S_usb_endpoint_descriptor)
+   , 0x05, 0x02, 0x02, (0x20)
+   , 0x00}
+
+};
+
+PROGMEM const S_usb_manufacturer_string_descriptor
+  usb_user_manufacturer_string_descriptor = {
+  sizeof(usb_user_manufacturer_string_descriptor)
+    , 0x03, {((U16) ('A')), ((U16) ('T')), ((U16) ('M')), ((U16) ('E')),
+             ((U16) ('L'))}
+};
+
+PROGMEM const S_usb_product_string_descriptor
+  usb_user_product_string_descriptor = {
+  sizeof(usb_user_product_string_descriptor)
+    , 0x03, {((U16) ('A')), ((U16) ('V')), ((U16) ('R')), ((U16) (' ')),
+             ((U16) ('U')), ((U16) ('S')), ((U16) ('B')), ((U16) (' ')),
+             ((U16) ('C')), ((U16) ('D')), ((U16) ('C')), ((U16) (' ')),
+             ((U16) ('D')), ((U16) ('E')), ((U16) ('M')), ((U16) ('O'))}
+};
+
+PROGMEM const S_usb_serial_number usb_user_serial_number = {
+  sizeof(usb_user_serial_number)
+    , 0x03, {((U16) ('1')), ((U16) ('.')), ((U16) ('0')), ((U16) ('.')),
+             ((U16) ('0'))}
+};
+
+PROGMEM const S_usb_language_id usb_user_language_id = {
+  sizeof(usb_user_language_id)
+    , 0x03, (0x0409)
+};
+
 int main(void)
 {
   (UHWCON |= (1 << UVREGE));
