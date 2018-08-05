b1c1f97fffca6ddecc9f7df0890c4d001f75f03f
Igor Liferenko <igor.liferenko@gmail.com>
2018-08-05 18:47:04 +0700

    cleanup main.c

--- main.c
+++ main.c
@@ -1,95 +1,17 @@
-typedef float Float16;
-
-typedef unsigned char U8;
-typedef unsigned short U16;
-typedef unsigned long U32;
-typedef signed char S8;
-typedef signed short S16;
-typedef long S32;
-
-typedef unsigned char Bool;
-
-typedef U8 Status;
-typedef Bool Status_bool;
-typedef unsigned char Uchar;
-
-typedef unsigned char Uint8;
-typedef unsigned int Uint16;
-typedef unsigned long int Uint32;
-
-typedef char Int8;
-typedef int Int16;
-typedef long int Int32;
-
-typedef unsigned char Byte;
-typedef unsigned int Word;
-typedef unsigned long int DWord;
-
-typedef union {
-  Uint32 dw;
-  Uint16 w[2];
-  Uint8 b[4];
-} Union32;
-
-typedef union {
-  Uint16 w;
-  Uint8 b[2];
-} Union16;
-typedef char p_uart_ptchar;
-typedef char r_uart_ptchar;
 #include  <avr/interrupt.h>
 #include  <avr/pgmspace.h>
 #include  <avr/io.h>
 #include  <stdio.h>
-U8 flash_read_sig(unsigned long adr);
-
-U8 flash_read_fuse(unsigned long adr);
-extern void usb_task_init(void);
-extern void cdc_task_init(void);
-extern void usb_task(void);
-extern void cdc_task(void);
-void scheduler_init(void);
-void scheduler_tasks(void);
-void scheduler(void);
-void scheduler_empty_fct(void);
 #include  <avr/io.h>
 #include  <avr/wdt.h>
 #include  <avr/power.h>
-void set_idle_mode(void);
-void set_power_down_mode(void);
-void set_adc_noise_reduction_mode(void);
-void set_power_save_mode(void);
-void set_standby_mode(void);
-void set_ext_standby_mode(void);
-void Clock_switch_external(void);
-void Clock_switch_internal(void);
-typedef enum endpoint_parameter { ep_num, ep_type, ep_direction, ep_size,
-  ep_bank, nyet_status
-} t_endpoint_parameter;
-U8 usb_config_ep(U8, U8);
-U8 usb_select_enpoint_interrupt(void);
-U16 usb_get_nb_byte_epw(void);
-U8 usb_send_packet(U8, U8 *, U8);
-U8 usb_read_packet(U8, U8 *, U8);
-void usb_halt_endpoint(U8);
-void usb_reset_endpoint(U8);
-U8 usb_init_device(void);
-U32 boot_key __attribute__ ((section(".noinit")));
-void start_boot_if_required(void);
-void start_boot(void);
 
-void (*start_bootloader) (void) = (void (*)(void)) 0x3800;
-extern U8 usb_remote_wup_feature;
-char uart_usb_getchar(void);
-int uart_usb_putchar(int);
-extern U8 usb_suspended;
-extern U8 usb_configuration_nb;
-volatile U16 g_usb_event = 0;
-extern U8 usb_connected;
-void usb_device_task(void);
-void usb_start_device(void);
-extern void sof_action(void);
-extern volatile U8 usb_request_break_generation;
+typedef unsigned char U8;
+typedef unsigned short U16;
+typedef unsigned long U32;
+typedef unsigned char Bool;
+typedef unsigned char Uchar;
+
 typedef union {
   U16 all;
   struct {
@@ -113,14 +35,6 @@ typedef union {
   };
 } S_line_status;
 S_line_status line_status;
-U8 uart_usb_test_hit(void);
-extern U8 rx_counter;
-int uart_putchar(int uc_wr_byte);
-volatile U8 cpt_sof;
-Bool cdc_update_serial_state();
-void usb_process_request(void);
-volatile U8 rs2usb[10];
-void uart_usb_send_buffer(U8 * buffer, U8 nb_data);
 typedef struct {
   U32 dwDTERate;
   U8 bCharFormat;
@@ -298,33 +212,28 @@ PROGMEM const S_usb_language_id usb_user_language_id = {
     , 0x03, (0x0409)
 };
 
+void (*start_bootloader) (void) = (void (*)(void)) 0x3800;
+int uart_usb_putchar(int);
+char uart_usb_getchar(void);
+U8 flash_read_fuse(unsigned long adr);
+Bool usb_user_read_request(U8, U8);
+
 U8 bmRequestType;
 U8 zlp;
 U8 endpoint_status[7];
 U8 device_status = 1;
-void usb_get_descriptor(void);
-void usb_set_address(void);
-void usb_set_configuration(void);
-void usb_clear_feature(void);
-void usb_set_feature(void);
-void usb_get_status(void);
-void usb_get_configuration(void);
-void usb_get_interface(void);
-void usb_set_interface(void);
-Bool usb_user_read_request(U8, U8);
-void usb_user_endpoint_init(U8);
 U8 data_to_transfer;
 PGM_VOID_P pbuffer;
 Bool usb_user_get_descriptor(U8, U8);
 U8 remote_wakeup_feature = 0;
 U8 usb_configuration_nb;
 U8 usb_remote_wup_feature;
-void cdc_get_line_coding();
-void cdc_set_control_line_state(U16);
-void cdc_send_break(U16);
-void cdc_set_line_coding();
 S_serial_state serial_state_saved;
 volatile U8 usb_request_break_generation = 0;
+volatile U8 rs2usb[10];
+volatile U8 cpt_sof;
+volatile U16 g_usb_event = 0;
+U32 boot_key __attribute__ ((section(".noinit")));
 
 int main(void)
 {
